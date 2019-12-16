9751985a519519bb977c092766de6da215d5f4b9
wx22mj <Jesse.Meng@m72a3.ncep.noaa.gov>
2019-12-16 16:45:01 +0000

    Jesse Meng 20191216 fix the bug in gldas_forcing.f for processing 0.125 cpc gauge data.

--- sorc/gldas_forcing.fd/gldas_forcing.f
+++ sorc/gldas_forcing.fd/gldas_forcing.f
@@ -1,40 +1,42 @@
-! ifort xie2014.F -free -L/nwprod/lib/ -lbacio_4 -lw3nco_4
+! ifort gfs_gdas_gldas_precip.f -free -L/nwprod/lib/ -lbacio_4 -lw3nco_4
 !- - - - -- - -- - -- - -- - - -- - --  -- - -- - -- - - -- - - - -- - --
-! This program creats 6hr 0.25 deg gldas precip forcing from
-! CPC PRCP_CU_GAUGE_V1.0GLB_0.25deg.lnx.20190101.RT
-! 6hr disaggregate with GDAS 6 hr precip
+! This program creats 6hr 0.125 deg gldas precip forcing from
+! CPC PRCP_CU_GAUGE_V1.0GLB_0.125deg.lnx.20190604.RT
+! 6hr disaggregate with GDAS 6 hr precip.
+! CPC PRCP_CU_GAUGE_V1.0GLB_0.125deg.lnx.20190604.RT covers
+! 12Z20190603 to 12Z20190604
 !
 ! input
 !  
-!   fort.11 gdas.precip.t00z.f06 copygb to 0.25 deg in bin
-!   fort.12 gdas.precip.t06z.f06 copygb to 0.25 deg in bin
-!   fort.13 gdas.precip.t12z.f06 copygb to 0.25 deg in bin
-!   fort.14 gdas.precip.t18z.f06 copygb to 0.25 deg in bin
-!   fort.15 PRCP_CU_GAUGE_V1.0GLB_0.25deg in bin
+!   fort.11 gdas.precip.t12z.f06 copygb to 0.125 deg in bin
+!   fort.12 gdas.precip.t18z.f06 copygb to 0.125 deg in bin
+!   fort.13 gdas.precip.t00z.f06 copygb to 0.125 deg in bin
+!   fort.14 gdas.precip.t06z.f06 copygb to 0.125 deg in bin
+!   fort.15 PRCP_CU_GAUGE_V1.0GLB_0.125deg.lnx in bin
 !  
 ! output
 !
-!   fort.21 precip.gldas.t00z.f06.grb 0.25 deg
-!   fort.22 precip.gldas.t00z.f06.grb 0.25 deg
-!   fort.23 precip.gldas.t00z.f06.grb 0.25 deg
-!   fort.24 precip.gldas.t00z.f06.grb 0.25 deg
+!   fort.21 precip.gldas.t12z.f06.grb 0.125 deg
+!   fort.22 precip.gldas.t18z.f06.grb 0.125 deg
+!   fort.23 precip.gldas.t00z.f06.grb 0.125 deg
+!   fort.24 precip.gldas.t06z.f06.grb 0.125 deg
 !
-! 20190509 Jesse Meng
+! 20190604 Jesse Meng
 ! - - - - -- - -- - -- - -- - - -- - --  -- - -- - -- - - -- - - - -- - --
 !
   implicit none
 
   integer :: i, j, k, j1, j2
-  integer, parameter :: mx = 1440
-  integer, parameter :: my = 720
+  integer, parameter :: mx = 2881
+  integer, parameter :: my = 1441
   real               :: data1(mx,my,4)    !N-S
   real               :: data1s(mx,my)     !N-S SUM data1
   real               :: data2(mx,my)      !S-N
   real               :: data2r(mx,my)     !N-S
 
-  integer            :: year
-  integer            :: month
-  integer            :: day
+  integer            :: year1, year2
+  integer            :: month1, month2
+  integer            :: day1, day2
   integer            :: hour
   integer            :: kpds(200)
   integer            :: kgds(200)
@@ -44,10 +46,12 @@
 ! GET DATE
 
   open(10,file='fort.10',status='old')
-  read(10,'(I4,I2,I2)') year, month, day
+  read(10,'(I4,I2,I2)') year1, month1, day1
+  read(10,'(I4,I2,I2)') year2, month2, day2
   close(10)
-  hour = 0
-  print*, year, month, day, hour
+  hour = 12
+  print*, year1, month1, day1
+  print*, year2, month2, day2
 
 ! PDS AND GDS
 
@@ -59,9 +63,9 @@
       kpds(5) = 59            !PRATE
       kpds(6) = 1             !SURFACE
       kpds(7) = 0             !LEVEL
-      kpds(8) = mod(year,100) !YY
-      kpds(9) = month         !MONTH
-      kpds(10)= day           !DAY
+      kpds(8) = mod(year1,100)!YY
+      kpds(9) = month1        !MONTH
+      kpds(10)= day1          !DAY
       kpds(11)= hour          !HOUR
       kpds(12)= 0             !MINUTE
       kpds(13)= 1             !FORECAST TIME UNIT
@@ -72,7 +76,7 @@
       kpds(18)= 0             !NOT USED
       kpds(19)= 130           !LDAS PARAMETER TABLE
       kpds(20)= 0             !MISSING
-      kpds(21)= year/100 + 1  !CENTURY
+      kpds(21)= year1/100 + 1 !CENTURY
       kpds(22)= 6             !DECIMAL
       kpds(23)= 4             !EMC
       kpds(24)= 0             !NOT USED
@@ -82,13 +86,13 @@
       kgds(1) = 0
       kgds(2) = mx
       kgds(3) = my
-      kgds(4) =   89875
-      kgds(5) =     125
+      kgds(4) =   90000
+      kgds(5) =       0
       kgds(6) = 128
-      kgds(7) =  -89875
-      kgds(8) =  359875
-      kgds(9) =     250
-      kgds(10)=     250
+      kgds(7) =  -90000
+      kgds(8) =  360000
+      kgds(9) =     125
+      kgds(10)=     125
       kgds(11)= 0
       kgds(12)= 0
       kgds(13)= 0
@@ -105,8 +109,8 @@
       kgds(24)= 0
       kgds(25)= 0
 
-  print*, kpds(1:25)
-  print*, kgds(1:25)
+!  print*, kpds(1:25)
+!  print*, kgds(1:25)
 
 ! GET 6 HR CFSR PRECIP.GLDAS
 
@@ -136,7 +140,7 @@
   enddo
   enddo
 
-! GET XIE PRECIP
+! GET CPC XIE PRECIP
 
   open (15,file='fort.15',form='unformatted',&
           access='direct',recl=mx*my, status='old')
@@ -144,7 +148,7 @@
   read (15,rec=1) data2
   close(15)
 
-  data2=data2/10.
+  data2=data2/10. !cpc data unit in 0.1mm
 
 ! YREV
   
@@ -179,7 +183,10 @@
 
 ! GRIBOUT
 
-  kpds(11) = 0
+  kpds(8) = mod(year1,100)!YY
+  kpds(9) = month1        !MONTH
+  kpds(10)= day1          !DAY
+  kpds(11)= 12            !HOUR
 
   bitmap = .TRUE.
   where ( data1(:,:,1) .LT. 0. )
@@ -198,7 +205,10 @@
   print*, 'iret =', iret
   print*
 
-  kpds(11) = 6
+  kpds(8) = mod(year1,100)!YY
+  kpds(9) = month1        !MONTH
+  kpds(10)= day1          !DAY
+  kpds(11)= 18            !HOUR
 
   bitmap = .TRUE.
   where ( data1(:,:,2) .LT. 0. )
@@ -217,7 +227,10 @@
   print*, 'iret =', iret
   print*
 
-  kpds(11) = 12
+  kpds(8) = mod(year2,100)!YY
+  kpds(9) = month2        !MONTH
+  kpds(10)= day2          !DAY
+  kpds(11)= 0             !HOUR
 
   bitmap = .TRUE.
   where ( data1(:,:,3) .LT. 0. )
@@ -236,7 +249,10 @@
   print*, 'iret =', iret
   print*
 
-  kpds(11) = 18
+  kpds(8) = mod(year2,100)!YY
+  kpds(9) = month2        !MONTH
+  kpds(10)= day2          !DAY
+  kpds(11)= 6             !HOUR
 
   bitmap = .TRUE.
   where ( data1(:,:,4) .LT. 0. )
