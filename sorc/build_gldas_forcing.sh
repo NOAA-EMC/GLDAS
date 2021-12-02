b6e2e864e7f47e018ba7ab277fddc5932a5b6e41
Helin Wei <Helin.Wei@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
2021-12-02 18:45:00 +0000

    LMOD conversion

--- sorc/build_gldas_forcing.sh
+++ sorc/build_gldas_forcing.sh
@@ -7,13 +7,19 @@ cwd=`pwd`
 USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
 if [ $USE_PREINST_LIBS = true ]; then
   export MOD_PATH
-  source ../modulefiles/gldas_forcing.$target             > /dev/null 2>&1
+  module purge
+  module use ../modulefiles
+  module load gldas_forcing.$target             > /dev/null 2>&1
 else
   export MOD_PATH=${cwd}/lib/modulefiles
   if [ $target = wcoss_cray ]; then
-    source ../modulefiles/gldas_forcing.${target}_userlib > /dev/null 2>&1
+    module purge
+    module use ../modulefiles
+    module load gldas_forcing.${target}_userlib > /dev/null 2>&1
   else
-    source ../modulefiles/gldas_forcing.$target           > /dev/null 2>&1
+    module purge
+    module use ../modulefiles
+    module load gldas_forcing.$target           > /dev/null 2>&1
   fi
 fi
 
@@ -26,6 +32,9 @@ fi
 # --- Chgres part
 #
 cd gldas_forcing.fd
+
+export FC=ftn
+export FOPTS="-O0 -FR"
 
 make clean
 make
