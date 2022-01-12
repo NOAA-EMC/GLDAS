06b5bf08b418ea18a42c9f72dc56d63decc992a7
HelinWei-NOAA <48133472+HelinWei-NOAA@users.noreply.github.com>
2022-01-12 14:05:54 -0500

    Wcoss2 nco (#35)
    * address CFP machine checks, jlogfile, compath
    
    * address CFP machine checks on gldas_forcing.sh
    
    * use ftn to replace mpifort on wcoss2
    
    * addressed the requested changes from NCO
    
    Co-authored-by: Helin Wei <Helin.Wei@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
    Co-authored-by: Helin Wei <Helin.Wei@dlogin03.dogwood.wcoss2.ncep.noaa.gov>

--- sorc/build_gldas_rst.sh
+++ sorc/build_gldas_rst.sh
@@ -7,17 +7,17 @@ cwd=`pwd`
 USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
 if [ $USE_PREINST_LIBS = true ]; then
   export MOD_PATH
-  module purge
+  module reset
   module use ../modulefiles
   module load gldas_rst.$target             > /dev/null 2>&1
 else
   export MOD_PATH=${cwd}/lib/modulefiles
   if [ $target = wcoss_cray ]; then
-    module purge
+    module reset
     module use ../modulefiles
     module load gldas_rst.${target}_userlib > /dev/null 2>&1
   else
-    module purge
+    module reset
     module use ../modulefiles
     module load gldas_rst.$target           > /dev/null 2>&1
   fi
