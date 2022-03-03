9415a7bce26f3615ac6a08b228387e0f183f8b19
Helin Wei <Helin.Wei@noaa.gov>
2022-03-03 12:41:48 -0500

    update modules to support GFSv16.2.0 on Hera

--- modulefiles/gldas_post.hera.lua
+++ modulefiles/gldas_post.hera.lua
@@ -2,18 +2,18 @@ help([[
 Load environment to run GLDAS on Hera
 ]])
 
-prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack-gfsv16/modulefiles/stack")
 
-hpc_ver=os.getenv("hpc_ver") or "1.1.0"
+hpc_ver=os.getenv("hpc_ver") or "1.2.0"
 load(pathJoin("hpc", hpc_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
+hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.2"
 load(pathJoin("hpc-intel", hpc_intel_ver))
 
-impi_ver=os.getenv("impi_ver") or "2018.0.4"
+impi_ver=os.getenv("impi_ver") or "2022.1.2"
 load(pathJoin("hpc-impi", impi_ver))
 
-w3emc_ver=os.getenv("w3emc_ver") or "2.7.3"
+w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
 w3nco_ver=os.getenv("w3nco_ver") or "2.4.1"
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
